971b334e9da3edaeedb3476d47b74e1d7efb88b0
IzzyDavid <david.irizarry86@yahoo.com>
2019-10-22 02:25:07 -0400

    make it better

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path");
+const mongoose = require("mongoose");
+var cookieParser = require('cookie-parser');
 const passport = require('passport');
-const config = require('./config');
+const config = require('./config/index.js');
+var jwt = require('express-jwt');
 
 // connect to the database and load models
 require('./server/models').connect(config.dbUri);
 
 const app = express();
+
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload',
+  getToken: function fromHeaderOrCookie(req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      console.log("auth", req.headers.authorization.split(' ')[1]);
+      return req.headers.authorization.split(' ')[1];
+    }
+    //  else if (req.cookies.token) {
+    //    console.log("cook", req.cookies.token)
+    //   return req.cookies.token
+    // }
+    return null;
+  }
+});
 // tell the app to look for static files in these directories
+
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
 // tell the app to parse HTTP body messages
@@ -30,6 +51,25 @@ const authRoutes = require('./server/routes/auth');
 const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/izzy-resume-finder-react";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//send react client folder for the front end
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 // Set Port, hosting services will look for process.env.PORT
 app.set('port', (process.env.PORT || 3000));
