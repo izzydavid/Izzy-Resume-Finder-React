574c4bc972275d7dbb582b9e78581a8fcc36023d
Ubernator <43094772+izzydavid@users.noreply.github.com>
2019-10-23 05:04:47 -0400

    make it better

--- routes/helpers/auth.helpers.js
+++ routes/helpers/auth.helpers.js
@@ -1,23 +1,56 @@
-var crypto = require('crypto');
-/**
- * 
- */
-var authenticationHelpers = {
-    getSalt() {
-        return crypto.randomBytes(16).toString('hex');
-    },
-    /**
-     * 
-     * @param {*} salt 
-     * @param {*} password 
-     */
-    getHash(salt, password) {
-        return crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512').toString('hex');
-    },
-    checkIfValidPass(user, password) {
-        var unvalidatedHash = authenticationHelpers.getHash(user.salt, password);
-        return unvalidatedHash === user.hash;
-    }
-};
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
-module.exports = authenticationHelpers;
+const User = require("../../models/user");
+
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
+
+  // basic validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields." });
+  }
+
+  // check for existing user
+  User.findOne({ email }).then(user => {
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+    // validate password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+      jwt.sign(
+        { id: user.id },
+        process.env.jwtSecret,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+            user: {
+              _id: user._id,
+              name: user.name,
+              email: user.email,
+              location: {
+                lat: user.location.lat,
+                lng: user.location.lng,
+                zip: user.location.zip,
+                city: user.location.city
+              }
+            }
+          });
+        }
+      );
+    });
+  });
+});
+
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then(user => res.json(user));
+});
+
+module.exports = router;
